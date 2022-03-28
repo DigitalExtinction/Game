a2a467f43029f8bff6eb387aae6a95a4d00bd70c
Martin Indra <martin.indra@mgn.cz>
2022-03-28 19:24:50 +0200

    Make the game fullscreen & add window title

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::WindowMode};
 
 fn main() {
     App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Digital Extinction".to_string(),
+            mode: WindowMode::BorderlessFullscreen,
+            ..Default::default()
+        })
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
