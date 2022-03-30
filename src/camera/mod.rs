5e90bccc34794858b7a84d91c6d71e5bd963c1ec
Martin Indra <martin.indra@mgn.cz>
2022-03-30 21:16:11 +0200

    Implement horizontal camera movement
    Relates to #1.

--- src/camera/mod.rs
+++ src/camera/mod.rs
@@ -1,17 +1,191 @@
-use crate::states::GameStates;
-use bevy::prelude::*;
+use crate::{
+    map::file::MapSize,
+    math::ray::{ray_aabb_intersection, ray_mesh_intersection, ray_plane_intersection, Ray},
+    states::GameStates,
+    terrain::Terrain,
+};
+use bevy::{prelude::*, render::primitives::Aabb};
+use glam::Vec3A;
+
+/// Horizontal camera movement is initiated if mouse cursor is within this
+/// distance to window edge.
+const MOVE_MARGIN_LOGICAL_PX: f32 = 40.;
+/// Camera moves horizontally at speed `distance * CAMERA_HORIZONTAL_SPEED`
+/// meters per second.
+const CAMERA_HORIZONTAL_SPEED: f32 = 1.0;
 
 pub struct MainCameraPlugin;
 
 impl Plugin for MainCameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system_set(SystemSet::on_enter(GameStates::InGame).with_system(setup));
+        app.add_event::<FocusInvalidatedEvent>()
+            .add_system_set(SystemSet::on_enter(GameStates::InGame).with_system(setup))
+            .add_system_set(
+                SystemSet::on_update(GameStates::InGame)
+                    .with_system(update_focus.label("update_focus"))
+                    .with_system(move_horizontaly_event.label("move_horizontaly_event"))
+                    .with_system(
+                        move_horizontaly
+                            .label("move_horizontaly")
+                            .after("move_horizontaly_event")
+                            .after("update_focus"),
+                    ),
+            );
+    }
+}
+
+struct CameraFocus {
+    point: Vec3,
+    distance: f32,
+}
+
+impl CameraFocus {
+    fn point(&self) -> Vec3 {
+        self.point
+    }
+
+    fn distance(&self) -> f32 {
+        self.distance
+    }
+
+    fn update<V: Into<Vec3>>(&mut self, point: V, distance: f32) {
+        self.point = point.into();
+        self.distance = distance;
+    }
+}
+
+struct FocusInvalidatedEvent;
+
+#[derive(Copy, Clone)]
+enum HorizontalMovementDirection {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Default)]
+struct HorizontalMovement {
+    movement: Option<HorizontalMovementDirection>,
+}
+
+impl HorizontalMovement {
+    fn movement(&self) -> Option<HorizontalMovementDirection> {
+        self.movement
+    }
+
+    fn start(&mut self, direction: HorizontalMovementDirection) {
+        self.movement = Some(direction);
+    }
+
+    fn stop(&mut self) {
+        self.movement = None;
     }
 }
 
 fn setup(mut commands: Commands) {
+    let initial_camera_distance = 10.0;
+    commands.insert_resource(HorizontalMovement::default());
+    commands.insert_resource(CameraFocus {
+        point: Vec3::ZERO,
+        distance: initial_camera_distance,
+    });
     commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(0.0, 5.0, 0.0).looking_at(Vec3::ZERO, -Vec3::Z),
+        transform: Transform::from_xyz(0.0, initial_camera_distance, 0.0)
+            .looking_at(Vec3::ZERO, -Vec3::Z),
         ..Default::default()
     });
+}
+
+fn update_focus(
+    mut event: EventReader<FocusInvalidatedEvent>,
+    mut focus: ResMut<CameraFocus>,
+    meshes: Res<Assets<Mesh>>,
+    camera_query: Query<&GlobalTransform, With<Camera>>,
+    terrain_query: Query<(&Handle<Mesh>, &GlobalTransform, &Aabb), With<Terrain>>,
+) {
+    if event.iter().next().is_none() {
+        return;
+    }
+
+    let camera_transform = camera_query.single();
+    let ray = Ray::new(camera_transform.translation, camera_transform.forward());
+
+    for (mesh_handle, terrain_transform, aabb) in terrain_query.iter() {
+        let mesh_to_world = terrain_transform.compute_matrix();
+        if ray_aabb_intersection(&ray, aabb, &mesh_to_world).is_none() {
+            continue;
+        }
+        let mesh = meshes.get(mesh_handle).unwrap();
+        if let Some(intersection) = ray_mesh_intersection(&ray, mesh, &mesh_to_world) {
+            focus.update(intersection.position(), intersection.distance());
+            return;
+        }
+    }
+
+    let intersection = ray_plane_intersection(&ray, Vec3A::ZERO, Vec3A::Y)
+        .expect("Camera ray does not intersect base ground plane.");
+    focus.update(intersection.position(), intersection.distance());
+}
+
+fn move_horizontaly(
+    horizontal_movement: Res<HorizontalMovement>,
+    focus: Res<CameraFocus>,
+    map_size: Res<MapSize>,
+    time: Res<Time>,
+    mut camera_query: Query<&mut Transform, With<Camera>>,
+    mut event: EventWriter<FocusInvalidatedEvent>,
+) {
+    let direction = match horizontal_movement.movement() {
+        Some(direction) => direction,
+        None => return,
+    };
+
+    let mut transform = camera_query.single_mut();
+    let delta_scalar = time.delta().as_secs_f32() * focus.distance() * CAMERA_HORIZONTAL_SPEED;
+    let delta_vec = match direction {
+        HorizontalMovementDirection::Left => -transform.local_x() * delta_scalar,
+        HorizontalMovementDirection::Right => transform.local_x() * delta_scalar,
+        HorizontalMovementDirection::Up => -transform.local_y() * delta_scalar,
+        HorizontalMovementDirection::Down => transform.local_y() * delta_scalar,
+    };
+
+    let min_delta_vec = Vec3::new(-focus.point().x.max(0.), 0., -focus.point().z.max(0.));
+    let max_delta_vec = Vec3::new(
+        (map_size.0 - focus.point().x).max(0.),
+        0.,
+        (map_size.0 - focus.point().z).max(0.),
+    );
+
+    transform.translation += delta_vec.clamp(min_delta_vec, max_delta_vec);
+    event.send(FocusInvalidatedEvent);
+}
+
+fn move_horizontaly_event(
+    mut horizontal_movement: ResMut<HorizontalMovement>,
+    windows: Res<Windows>,
+) {
+    let window = windows.get_primary().unwrap();
+    let (x, y) = match window.cursor_position() {
+        Some(position) => (position.x, position.y),
+        None => {
+            horizontal_movement.stop();
+            return;
+        }
+    };
+
+    let width = window.width();
+    let height = window.height();
+
+    if x < MOVE_MARGIN_LOGICAL_PX {
+        horizontal_movement.start(HorizontalMovementDirection::Left);
+    } else if x > (width - MOVE_MARGIN_LOGICAL_PX) {
+        horizontal_movement.start(HorizontalMovementDirection::Right);
+    } else if y < MOVE_MARGIN_LOGICAL_PX {
+        horizontal_movement.start(HorizontalMovementDirection::Up);
+    } else if y > (height - MOVE_MARGIN_LOGICAL_PX) {
+        horizontal_movement.start(HorizontalMovementDirection::Down);
+    } else {
+        horizontal_movement.stop();
+    }
 }
