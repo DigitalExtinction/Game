ef7b7f976d3fbb1c3bfd9fb0a90e1050222fa4ff
Martin Indra <martin.indra@mgn.cz>
2022-03-30 16:31:29 +0200

    Add ray mesh intersection benchmark
    I takes ~0.34ms on Intel(R) Core(TM) i5-8265U CPU @ 1.60GHz to compute
    intersection with a mesh with 20k triangles.

---
+++ benches/ray_intersection.rs
@@ -0,0 +1,60 @@
+use bevy::{
+    prelude::Mesh,
+    render::{mesh::Indices, render_resource::PrimitiveTopology},
+};
+use criterion::{criterion_group, criterion_main, Criterion};
+use de::math::ray::{ray_mesh_intersection, Ray};
+use glam::{Mat4, Vec3A};
+
+fn generate_mesh(grid_size: usize) -> Mesh {
+    let mut positions = Vec::new();
+    let mut normals = Vec::new();
+    let mut uvs = Vec::new();
+    let scale = 1. / grid_size as f32;
+    for row in 0..grid_size {
+        for column in 0..grid_size {
+            let x1 = column as f32 * scale;
+            let x2 = x1 + scale;
+            let z1 = row as f32 * scale;
+            let z2 = z1 + scale;
+            positions.push([x1, 0., z1]);
+            uvs.push([x1, z1]);
+            normals.push([0., 1., 0.]);
+            positions.push([x1, 0., z2]);
+            uvs.push([x1, z2]);
+            normals.push([0., 1., 0.]);
+            positions.push([x2, 0., z2]);
+            uvs.push([x2, z2]);
+            normals.push([0., 1., 0.]);
+            positions.push([x1, 0., z1]);
+            uvs.push([x1, z1]);
+            normals.push([0., 1., 0.]);
+            positions.push([x2, 0., z2]);
+            uvs.push([x2, z2]);
+            normals.push([0., 1., 0.]);
+            positions.push([x2, 0., z1]);
+            uvs.push([x2, z1]);
+            normals.push([0., 1., 0.]);
+        }
+    }
+
+    let indices = Indices::U32((0..positions.len()).map(|i| i as u32).collect());
+    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+    mesh.set_indices(Some(indices));
+    mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+    mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+    mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+    mesh
+}
+
+fn mesh_intersection_benchmark(c: &mut Criterion) {
+    c.bench_function("Ray Mesh Intersection", |b| {
+        let ray = Ray::new(Vec3A::Y, -Vec3A::Y);
+        let mesh = generate_mesh(100);
+        let mesh_to_world = Mat4::from_rotation_z(1.);
+        b.iter(|| ray_mesh_intersection(&ray, &mesh, &mesh_to_world))
+    });
+}
+
+criterion_group!(benches, mesh_intersection_benchmark);
+criterion_main!(benches);
