0c88e25e0a05331ce48721c92fdd91b92523beca
Martin Indra <martin.indra@mgn.cz>
2022-04-26 16:20:12 +0200

    Implement map storing
    Relates to #43.

--- rust/map/src/io.rs
+++ rust/map/src/io.rs
@@ -1,8 +1,8 @@
 use crate::description::{Map, MapValidationError};
-use async_std::fs::File;
+use async_std::fs::{File, OpenOptions};
 use async_std::path::Path;
 use async_std::prelude::*;
-use async_tar::Archive;
+use async_tar::{Archive, Builder, EntryType, Header};
 use std::io;
 use thiserror::Error;
 
@@ -11,6 +11,15 @@ macro_rules! loading_io_error {
         match $expression {
             Ok(value) => value,
             Err(err) => return Err(MapLoadingError::Io { source: err }),
+        }
+    };
+}
+
+macro_rules! storing_io_error {
+    ($expression:expr) => {
+        match $expression {
+            Ok(value) => value,
+            Err(err) => return Err(MapStoringError::Io { source: err }),
         }
     };
 }
@@ -62,4 +71,90 @@ pub enum MapLoadingError {
     JsonParsing { source: serde_json::Error },
     #[error(transparent)]
     Validation { source: MapValidationError },
+}
+
+/// Writes a map to a TAR file. Overwrites the file if it already exists.
+pub async fn store_map<P: AsRef<Path>>(map: &Map, path: P) -> Result<(), MapStoringError> {
+    let file = storing_io_error!(
+        OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .create(true)
+            .open(path)
+            .await
+    );
+
+    let mut archive = Builder::new(file);
+
+    let map_data = match serde_json::ser::to_vec(map) {
+        Ok(data) => data,
+        Err(error) => return Err(MapStoringError::JsonSerialization { source: error }),
+    };
+    let mut map_header = Header::new_gnu();
+    map_header.set_entry_type(EntryType::Regular);
+    map_header.set_mode(0x400);
+    map_header.set_size(map_data.len() as u64);
+    storing_io_error!(
+        archive
+            .append_data(
+                &mut map_header,
+                Path::new(MAP_JSON_ENTRY),
+                map_data.as_slice(),
+            )
+            .await
+    );
+
+    Ok(())
+}
+
+#[derive(Error, Debug)]
+pub enum MapStoringError {
+    #[error(transparent)]
+    Io { source: io::Error },
+    #[error("map JSON serialization error")]
+    JsonSerialization { source: serde_json::Error },
+}
+
+#[cfg(test)]
+mod test {
+    use async_std::task;
+    use de_objects::{ActiveObjectType, Player};
+    use glam::Vec2;
+    use std::path::PathBuf;
+    use tempfile::Builder;
+
+    use super::*;
+    use crate::{
+        description::{ActiveObject, Map, Object, ObjectType},
+        size::MapBounds,
+    };
+
+    #[test]
+    fn test_store_load() {
+        let bounds = MapBounds::new(Vec2::new(1000., 2000.));
+        let mut map = Map::empty(bounds, Player::Player4);
+
+        let bases = [
+            (Vec2::new(100., 100.), Player::Player1),
+            (Vec2::new(900., 100.), Player::Player2),
+            (Vec2::new(900., 1900.), Player::Player3),
+            (Vec2::new(100., 1900.), Player::Player4),
+        ];
+
+        for (base_position, player) in bases {
+            map.insert_object(Object::new(
+                map.new_placement(base_position, 0.),
+                ObjectType::Active(ActiveObject::new(ActiveObjectType::Base, player)),
+            ));
+        }
+
+        let tmp_dir = Builder::new().prefix("de_map_").tempdir().unwrap();
+        let mut tmp_dir_path = PathBuf::from(tmp_dir.path());
+        tmp_dir_path.push("test-map.tar");
+
+        task::block_on(store_map(&map, tmp_dir_path.as_path())).unwrap();
+        let loaded_map = task::block_on(load_map(tmp_dir_path.as_path())).unwrap();
+
+        assert_eq!(loaded_map.bounds().max(), Vec2::new(1000., 2000.));
+    }
 }
