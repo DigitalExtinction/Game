6f26e1b19bfec2e253d51b77a9d00ed83942e139
Martin Indra <martin.indra@mgn.cz>
2022-04-29 15:36:13 +0200

    Move some code

--- crates/game/src/lib.rs
+++ crates/game/src/lib.rs
@@ -1,18 +1,2 @@
-use crate::game::GamePluginGroup;
-use bevy::{prelude::*, window::WindowMode};
-
 pub mod game;
 pub mod math;
-
-pub fn start() {
-    App::new()
-        .insert_resource(WindowDescriptor {
-            title: "Digital Extinction".to_string(),
-            mode: WindowMode::BorderlessFullscreen,
-            ..Default::default()
-        })
-        .insert_resource(Msaa { samples: 4 })
-        .add_plugins(DefaultPlugins)
-        .add_plugins(GamePluginGroup)
-        .run();
-}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use bevy::{prelude::*, window::WindowMode};
+use de_game::game::GamePluginGroup;
+
 fn main() {
-    de_game::start();
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Digital Extinction".to_string(),
+            mode: WindowMode::BorderlessFullscreen,
+            ..Default::default()
+        })
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_plugins(GamePluginGroup)
+        .run();
 }
