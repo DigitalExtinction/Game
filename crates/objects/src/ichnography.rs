1c11f3ddeb7b1300eb8dbab77e077b6b99b77151
Martin Indra <martin.indra@mgn.cz>
2022-10-06 18:56:17 +0200

    Add local_aabb() and convex_hull() to Ichnography

--- crates/objects/src/ichnography.rs
+++ crates/objects/src/ichnography.rs
@@ -1,5 +1,5 @@
 use de_core::objects::ObjectType;
-use parry2d::{math::Point, shape::ConvexPolygon};
+use parry2d::{bounding_volume::AABB, math::Point, shape::ConvexPolygon};
 
 use crate::{loader::Footprint, ObjectCache};
 
@@ -18,12 +18,30 @@ impl IchnographyCache for ObjectCache {
 }
 
 pub struct Ichnography {
+    local_aabb: AABB,
+    convex_hull: ConvexPolygon,
     offset_convex_hull: ConvexPolygon,
 }
 
 impl Ichnography {
-    pub fn new(offset_convex_hull: ConvexPolygon) -> Self {
-        Self { offset_convex_hull }
+    fn new(
+        local_aabb: AABB,
+        convex_hull: ConvexPolygon,
+        offset_convex_hull: ConvexPolygon,
+    ) -> Self {
+        Self {
+            local_aabb,
+            convex_hull,
+            offset_convex_hull,
+        }
+    }
+
+    pub fn local_aabb(&self) -> AABB {
+        self.local_aabb
+    }
+
+    pub fn convex_hull(&self) -> &ConvexPolygon {
+        &self.convex_hull
     }
 
     pub fn offset_convex_hull(&self) -> &ConvexPolygon {
@@ -31,17 +49,63 @@ impl Ichnography {
     }
 }
 
+impl From<ConvexPolygon> for Ichnography {
+    fn from(footprint: ConvexPolygon) -> Self {
+        let local_aabb = footprint.local_aabb();
+        let offset = footprint.offsetted(EXCLUSION_OFFSET);
+        Self::new(local_aabb, footprint, offset)
+    }
+}
+
 impl From<&Footprint> for Ichnography {
     fn from(footprint: &Footprint) -> Self {
-        let footprint = ConvexPolygon::from_convex_polyline(
+        ConvexPolygon::from_convex_polyline(
             footprint
                 .convex_hull()
                 .iter()
                 .map(|&[x, y]| Point::new(x, y))
                 .collect(),
         )
-        .unwrap();
+        .unwrap()
+        .into()
+    }
+}
 
-        Self::new(footprint.offsetted(EXCLUSION_OFFSET))
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ichnography_from() {
+        let footpring = ConvexPolygon::from_convex_polyline(vec![
+            Point::new(15., 125.),
+            Point::new(20., 125.),
+            Point::new(20., 225.),
+            Point::new(15., 225.),
+        ])
+        .unwrap();
+        let ichnography = Ichnography::from(footpring);
+        assert_eq!(
+            ichnography.local_aabb(),
+            AABB::new(Point::new(15., 125.), Point::new(20., 225.),)
+        );
+        assert_eq!(
+            ichnography.convex_hull().points(),
+            &[
+                Point::new(15., 125.),
+                Point::new(20., 125.),
+                Point::new(20., 225.),
+                Point::new(15., 225.),
+            ]
+        );
+        assert_eq!(
+            ichnography.offset_convex_hull().points(),
+            &[
+                Point::new(13., 123.),
+                Point::new(22., 123.),
+                Point::new(22., 227.),
+                Point::new(13., 227.),
+            ]
+        );
     }
 }
