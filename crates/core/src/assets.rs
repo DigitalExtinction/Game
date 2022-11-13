f5b0290d8f42620d33722e7997cb24b639bcba92
Martin Indra <martin.indra@mgn.cz>
2022-11-13 16:32:05 +0100

    de_core: Improve asset_path()

--- crates/core/src/assets.rs
+++ crates/core/src/assets.rs
@@ -3,15 +3,20 @@ use std::{
     path::{Path, PathBuf},
 };
 
-/// Converts a relative asset path to an absolute path.
+/// Converts a path relative to assets directory to an absolute path.
 ///
 /// If the game is executed with Cargo, the path is interpreted as relative to
 /// assets/ directory in the directory with Cargo manifest file.
 ///
 /// Otherwise, it is interpreted as relative to assets/ directory in the
 /// directory with the binary.
+///
+/// # Panics
+///
+/// Panics if `path` is not relative.
 pub fn asset_path<P: AsRef<Path>>(path: P) -> PathBuf {
     let path = path.as_ref();
+    assert!(path.is_relative(), "Asset path is not relative: {:?}", path);
     let mut new_path = match env::var("CARGO_MANIFEST_DIR") {
         Ok(path) => PathBuf::from(path),
         Err(_) => {
