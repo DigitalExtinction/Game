37d720da4e8cc8767e737d8352e2087ae21abc86
Martin Indra <martin.indra@mgn.cz>
2023-01-04 19:32:10 +0100

    Add Multiplayer to the main menu
    Relates to #301.

--- crates/menu/src/mainmenu.rs
+++ crates/menu/src/mainmenu.rs
@@ -15,23 +15,64 @@ impl Plugin for MainMenuPlugin {
     }
 }
 
+#[derive(Component, Clone, Copy)]
+struct ButtonAction(MenuState);
+
 fn setup(mut commands: GuiCommands) {
-    commands.spawn_button(
-        OuterStyle {
-            size: Size::new(Val::Percent(25.), Val::Percent(10.)),
-            margin: UiRect::all(Val::Auto),
-        },
-        "Start Game",
+    let root_node = commands
+        .spawn(NodeBundle {
+            style: Style {
+                flex_direction: FlexDirection::Column,
+                size: Size::new(Val::Percent(25.), Val::Percent(100.)),
+                margin: UiRect::all(Val::Auto),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                ..default()
+            },
+            ..default()
+        })
+        .id();
+
+    button(
+        &mut commands,
+        root_node,
+        ButtonAction(MenuState::MapSelection),
+        "Singleplayer",
     );
+    button(
+        &mut commands,
+        root_node,
+        ButtonAction(MenuState::SignIn),
+        "Multiplayer",
+    );
+}
+
+fn button(commands: &mut GuiCommands, parent: Entity, action: ButtonAction, caption: &str) {
+    let button = commands
+        .spawn_button(
+            OuterStyle {
+                size: Size::new(Val::Percent(100.), Val::Percent(8.)),
+                margin: UiRect::new(
+                    Val::Percent(0.),
+                    Val::Percent(0.),
+                    Val::Percent(2.),
+                    Val::Percent(2.),
+                ),
+            },
+            caption,
+        )
+        .insert(action)
+        .id();
+    commands.entity(parent).add_child(button);
 }
 
 fn button_system(
     mut commands: Commands,
-    interactions: Query<&Interaction, (Changed<Interaction>, With<Button>)>,
+    interactions: Query<(&Interaction, &ButtonAction), Changed<Interaction>>,
 ) {
-    for &interaction in interactions.iter() {
+    for (&interaction, &action) in interactions.iter() {
         if let Interaction::Clicked = interaction {
-            commands.insert_resource(NextState(MenuState::MapSelection));
+            commands.insert_resource(NextState(action.0));
         }
     }
 }
