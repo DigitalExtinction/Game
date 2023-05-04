49c80e23bace2288a94dbb4256ac4a9e1f212245
jackc <jackcrumpleys@gmail.com>
2023-05-05 11:35:09 +1200

    add logging to file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use bevy::log::LogPlugin;
 #[cfg(not(target_os = "macos"))]
 use bevy::window::{CursorGrabMode, PrimaryWindow};
 use bevy::{
@@ -16,6 +17,7 @@ use de_gui::GuiPluginGroup;
 use de_index::IndexPluginGroup;
 use de_loader::LoaderPluginGroup;
 use de_lobby_client::LobbyClientPluginGroup;
+use de_log::LogPluginGroup;
 use de_menu::MenuPluginGroup;
 use de_movement::MovementPluginGroup;
 use de_objects::ObjectsPluginGroup;
@@ -23,48 +25,90 @@ use de_pathing::PathingPluginGroup;
 use de_signs::SignsPluginGroup;
 use de_spawner::SpawnerPluginGroup;
 use de_terrain::TerrainPluginGroup;
+use tracing::{span, Level};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::{fmt, EnvFilter};
 
 const CARGO_PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
 const GIT_SHA: &str = env!("GIT_SHA");
 
 fn main() {
-    let mut app = App::new();
-    app.insert_resource(Msaa::Sample4)
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Digital Extinction".to_string(),
-                mode: WindowMode::BorderlessFullscreen,
-                ..Default::default()
-            }),
-            ..default()
-        }))
-        .add_plugin(LogDiagnosticsPlugin::default())
-        .add_plugin(FrameTimeDiagnosticsPlugin::default())
-        .add_plugin(GamePlugin)
-        .add_plugins(ConfigPluginGroup)
-        .add_plugins(GuiPluginGroup)
-        .add_plugins(LobbyClientPluginGroup)
-        .add_plugins(MenuPluginGroup)
-        .add_plugins(CorePluginGroup)
-        .add_plugins(ObjectsPluginGroup)
-        .add_plugins(TerrainPluginGroup)
-        .add_plugins(LoaderPluginGroup)
-        .add_plugins(IndexPluginGroup)
-        .add_plugins(PathingPluginGroup)
-        .add_plugins(SignsPluginGroup)
-        .add_plugins(SpawnerPluginGroup)
-        .add_plugins(MovementPluginGroup)
-        .add_plugins(ControllerPluginGroup)
-        .add_plugins(CameraPluginGroup)
-        .add_plugins(BehaviourPluginGroup)
-        .add_plugins(CombatPluginGroup)
-        .add_plugins(ConstructionPluginGroup);
+    // for file name
+    let dt = chrono::Local::now();
 
-    // This has to be after LogPlugin is inserted.
+    let file_name = dt.format("%Y-%m-%d %H:%M:%S.log").to_string();
+    let file_appender = tracing_appender::rolling::never("logs", &*file_name);
+
+    let (non_blocking_log_writer, _guard) = tracing_appender::non_blocking(file_appender);
+
+    let collector = tracing_subscriber::registry()
+        .with(
+            EnvFilter::from_default_env()
+                .add_directive(Level::TRACE.into())
+                .add_directive("wgpu_core=warn".parse().unwrap())
+                .add_directive("bevy_ecs=info".parse().unwrap())
+                .add_directive("async_io=info".parse().unwrap())
+                .add_directive("polling=debug".parse().unwrap())
+                .add_directive("wgpu_hal=info".parse().unwrap())
+                .add_directive("naga=info".parse().unwrap()),
+        )
+        .with(fmt::layer().with_writer(std::io::stdout))
+        .with(fmt::layer().with_writer(non_blocking_log_writer));
+    tracing::subscriber::set_global_default(collector).expect("Unable to set a global collector");
+
+    info!("Logger initialized");
+
     info!(
         "Starting Digital Extinction {{ \"Version\": \"{}\", \"GitSha\": \"{}\" }}",
         CARGO_PKG_VERSION, GIT_SHA
     );
+
+    // so we can use it after span is closed
+    let mut app: App;
+
+    {
+        let span = span!(Level::TRACE, "Startup");
+        let _enter = span.enter();
+
+        app = App::new();
+        app.insert_resource(Msaa::Sample4)
+            .add_plugins(LogPluginGroup {
+                current_log_file: file_name,
+            })
+            .add_plugins(
+                DefaultPlugins
+                    .set(WindowPlugin {
+                        primary_window: Some(Window {
+                            title: "Digital Extinction".to_string(),
+                            mode: WindowMode::BorderlessFullscreen,
+                            ..Default::default()
+                        }),
+                        ..default()
+                    })
+                    .disable::<LogPlugin>(),
+            )
+            .add_plugin(LogDiagnosticsPlugin::default())
+            .add_plugin(FrameTimeDiagnosticsPlugin::default())
+            .add_plugin(GamePlugin)
+            .add_plugins(ConfigPluginGroup)
+            .add_plugins(GuiPluginGroup)
+            .add_plugins(LobbyClientPluginGroup)
+            .add_plugins(MenuPluginGroup)
+            .add_plugins(CorePluginGroup)
+            .add_plugins(ObjectsPluginGroup)
+            .add_plugins(TerrainPluginGroup)
+            .add_plugins(LoaderPluginGroup)
+            .add_plugins(IndexPluginGroup)
+            .add_plugins(PathingPluginGroup)
+            .add_plugins(SignsPluginGroup)
+            .add_plugins(SpawnerPluginGroup)
+            .add_plugins(MovementPluginGroup)
+            .add_plugins(ControllerPluginGroup)
+            .add_plugins(CameraPluginGroup)
+            .add_plugins(BehaviourPluginGroup)
+            .add_plugins(CombatPluginGroup)
+            .add_plugins(ConstructionPluginGroup);
+    }
 
     app.run();
 }
