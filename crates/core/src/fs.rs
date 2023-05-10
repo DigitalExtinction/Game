46001e842022d229981946f27490802c57a51a51
Martin Indra <martin.indra@mgn.cz>
2023-05-10 10:10:05 +0200

    de_log: Log to an appropriate directory

--- crates/core/src/fs.rs
+++ crates/core/src/fs.rs
@@ -1,16 +1,29 @@
-use async_std::path::PathBuf;
+use std::path::PathBuf as SyncPathBuf;
+
+use async_std::path::PathBuf as AsyncPathBuf;
 use thiserror::Error;
 
 /// Returns DE configuration directory.
-pub fn conf_dir() -> Result<PathBuf, DirError> {
-    let Some(base_conf_dir) = dirs::config_dir() else {
-        return Err(
-            DirError("User's configuration directory cannot be established.")
+pub fn conf_dir() -> Result<AsyncPathBuf, DirError> {
+    dir(dirs::config_dir)
+}
 
-        );
+/// Returns DE logging directory.
+pub fn logs_dir() -> Result<AsyncPathBuf, DirError> {
+    dir(dirs::cache_dir).map(|d| d.join("logs"))
+}
+
+fn dir<F>(base_dir: F) -> Result<AsyncPathBuf, DirError>
+where
+    F: Fn() -> Option<SyncPathBuf>,
+{
+    let Some(base_dir) = base_dir() else {
+        return Err(
+            DirError("Base directory cannot be established.")
+       );
     };
 
-    Ok(PathBuf::from(base_conf_dir).join("DigitalExtinction"))
+    Ok(AsyncPathBuf::from(base_dir).join("DigitalExtinction"))
 }
 
 #[derive(Error, Debug)]
