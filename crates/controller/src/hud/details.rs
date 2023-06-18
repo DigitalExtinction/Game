fe9ced1dc30f5f43dc145f0b9c6609878cdd54bd
jack crump-leys <jackcrumpleys@gmail.com>
2023-06-18 12:07:26 +1200

    Display battery in details.

--- crates/controller/src/hud/details.rs
+++ crates/controller/src/hud/details.rs
@@ -1,37 +1,113 @@
 use bevy::prelude::*;
+use de_core::baseset::GameSet;
 use de_core::{cleanup::DespawnOnGameExit, gamestate::GameState};
+use de_energy::Battery;
+use de_gui::{BodyTextCommands, BodyTextOps, GuiCommands, OuterStyle};
 
 use super::{interaction::InteractionBlocker, HUD_COLOR};
+use crate::selection::Selected;
 
 pub(crate) struct DetailsPlugin;
 
 impl Plugin for DetailsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(spawn_details.in_schedule(OnEnter(GameState::Playing)));
+        app.add_system(setup.in_schedule(OnEnter(GameState::Playing)))
+            .add_system(
+                update
+                    .in_base_set(GameSet::PostUpdate)
+                    .run_if(in_state(GameState::Playing)),
+            );
     }
 }
 
-fn spawn_details(mut commands: Commands) {
-    commands.spawn((
-        NodeBundle {
-            style: Style {
+#[derive(Resource)]
+struct DetailsText(Entity);
+
+fn setup(mut commands: GuiCommands) {
+    let node = commands
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    size: Size {
+                        width: Val::Percent(20.),
+                        height: Val::Percent(30.),
+                    },
+                    position_type: PositionType::Absolute,
+                    position: UiRect::new(
+                        Val::Percent(0.),
+                        Val::Percent(20.),
+                        Val::Percent(70.),
+                        Val::Percent(100.),
+                    ),
+                    ..default()
+                },
+                background_color: HUD_COLOR.into(),
+                ..default()
+            },
+            DespawnOnGameExit,
+            InteractionBlocker,
+        ))
+        .id();
+    let details_text = commands
+        .spawn_body_text(
+            OuterStyle {
                 size: Size {
                     width: Val::Percent(20.),
                     height: Val::Percent(30.),
                 },
-                position_type: PositionType::Absolute,
-                position: UiRect::new(
-                    Val::Percent(0.),
-                    Val::Percent(20.),
-                    Val::Percent(70.),
-                    Val::Percent(100.),
-                ),
-                ..default()
+                margin: UiRect {
+                    left: Val::Px(10.),
+                    right: Val::Px(10.),
+                    top: Val::Px(10.),
+                    bottom: Val::Px(10.),
+                },
             },
-            background_color: HUD_COLOR.into(),
-            ..default()
-        },
-        DespawnOnGameExit,
-        InteractionBlocker,
-    ));
+            "",
+        )
+        .id();
+    commands.entity(node).add_child(details_text);
+
+    commands.insert_resource(DetailsText(details_text));
+}
+
+fn update(
+    ui: Res<DetailsText>,
+    selected: Query<Entity, With<Selected>>,
+    battery: Query<&Battery>,
+    mut text_ops: BodyTextOps,
+) {
+    if selected.is_empty() {
+        text_ops
+            .set_text(ui.0, "".into())
+            .expect("Failed to set text of details");
+        return;
+    }
+
+    let mut battery_total = 0;
+    let mut battery_max = 0;
+
+    for entity in selected.iter() {
+        if let Ok(battery) = battery.get(entity) {
+            battery_total += battery.energy().floor() as i64;
+            battery_max += battery.capacity().floor() as i64;
+        }
+    }
+
+    if battery_max == 0 {
+        return;
+    }
+
+    let text = format!(
+        "Battery: {}/{} ({}%)\nSelected {}",
+        battery_total,
+        battery_max,
+        battery_total * 100 / battery_max,
+        selected.iter().count()
+    );
+
+    text_ops
+        .set_text(ui.0, text)
+        .expect("Failed to set text of details");
+    #[derive(Resource)]
+    struct DetailsText(Entity);
 }
---
+++ crates/gui/src/body_text.rs
@@ -0,0 +1,81 @@
+use bevy::{
+    ecs::system::{EntityCommands, SystemParam},
+    prelude::*,
+};
+
+use crate::{GuiCommands, OuterStyle};
+
+/// marker component for UI `BasicText`
+#[derive(Component)]
+pub struct BodyText;
+
+pub trait BodyTextCommands<'w, 's> {
+    fn spawn_body_text<'a>(
+        &'a mut self,
+        size: OuterStyle,
+        caption: impl Into<String>,
+    ) -> EntityCommands<'w, 's, 'a>;
+}
+
+impl<'w, 's> BodyTextCommands<'w, 's> for GuiCommands<'w, 's> {
+    fn spawn_body_text<'a>(
+        &'a mut self,
+        style: OuterStyle,
+        caption: impl Into<String>,
+    ) -> EntityCommands<'w, 's, 'a> {
+        let text_style = self.text_props().label_text_style();
+
+        let mut commands = self.spawn((
+            NodeBundle {
+                style: Style {
+                    justify_content: JustifyContent::FlexStart,
+                    align_items: AlignItems::FlexStart,
+                    size: style.size,
+                    margin: style.margin,
+                    ..default()
+                },
+                ..default()
+            },
+            BodyText,
+        ));
+
+        commands.with_children(|builder| {
+            builder.spawn(TextBundle::from_section(caption, text_style));
+        });
+
+        info!("spawned body text");
+
+        commands
+    }
+}
+
+#[derive(SystemParam)]
+pub struct BodyTextOps<'w, 's> {
+    body_text_query: Query<'w, 's, &'static Children, With<BodyText>>,
+    text_query: Query<'w, 's, &'static mut Text>,
+}
+
+impl<'w, 's> BodyTextOps<'w, 's> {
+    /// This method changes text (e.g. caption) of UI body text.
+    ///
+    /// The entity must have [`BodyText`] component and at least one child with
+    /// [`Text`] component. The text must consist of a single section. If such
+    /// a child is found, its text is changed.
+    pub fn set_text(&mut self, entity: Entity, text: String) -> Result<(), &str> {
+        info!("setting body text");
+        let children = match self.body_text_query.get(entity) {
+            Ok(children) => children,
+            Err(e) => {
+                error!("BodyText does not exist. {:?}", e);
+                return Err("BodyText does not exist.");
+            }
+        };
+        for &child in children.iter() {
+            if let Ok(mut text_component) = self.text_query.get_mut(child) {
+                text_component.sections[0].value = text;
+                return Ok(());
+            }
+        }
+        Err("BodyText does not have a child with Text component.")
+    }
+}
