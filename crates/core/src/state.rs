ebca2548caef9b2ed55db6f67aefd4923f16224c
Martin Indra <martin.indra@mgn.cz>
2023-07-05 12:16:35 +0200

    Add AppState docs

--- crates/core/src/state.rs
+++ crates/core/src/state.rs
@@ -2,11 +2,24 @@ use bevy::prelude::{States, SystemSet};
 
 use crate::transition::StateWithSet;
 
+/// High level state of the application.
+///
+/// The application might enter each state multiple times. For example when the
+/// user finishes a game and then starts a new one.
+///
+/// Some sub-systems (e.g. menu, game) control different finer-grained states
+/// during each application state.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default, States)]
 pub enum AppState {
     #[default]
     AppLoading,
     InMenu,
+    /// A game has started.
+    ///
+    /// It may be loading, being played or finalizing.
+    ///
+    /// Before a game is started, make sure it is properly configured. Resource
+    /// [`crate::gconfig::GameConfig`] must exist.
     InGame,
 }
 
