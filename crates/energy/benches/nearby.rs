56e5130dfd988ff1d083398a7643eda2e29620a8
jack crump-leys <jackcrumpleys@gmail.com>
2023-08-03 14:49:10 +1200

    implement fixes

--- crates/energy/benches/nearby.rs
+++ crates/energy/benches/nearby.rs
@@ -1,7 +1,13 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
+
+use bevy::ecs::schedule::ScheduleLabel;
 use bevy::prelude::{
-    App, Changed, Component, Entity, Query, ResMut, Resource, Time, Timer, Transform, Update, World,
+    App, Changed, Component, Entity, Query, Res, ResMut, Schedule, Time, Transform, Update, Vec2,
+    World,
 };
-use bevy::time::{TimePlugin, TimerMode};
+use bevy::time::TimePlugin;
 use criterion::{criterion_group, criterion_main, Criterion};
 use de_energy::{update_nearby_recv, EnergyReceiver, NearbyUnits};
 use de_index::{EntityIndex, LocalCollider};
@@ -9,34 +15,18 @@ use de_objects::ObjectCollider;
 use parry3d::math::{Isometry, Vector};
 use parry3d::shape::{Cuboid, TriMesh};
 
-fn init_world_with_entities(world: &mut World, num_entities: usize) {
-    let mut index = EntityIndex::new();
+const MAP_SIZE: f32 = 2000.;
+const DISTANCE_FROM_MAP_EDGE: f32 = 100.;
 
-    for _ in 0..num_entities {
-        let x: f32 = fastrand::i32(-500..500) as f32;
-        let y: f32 = fastrand::i32(-500..500) as f32;
+#[derive(ScheduleLabel, Clone, Debug, PartialEq, Eq, Hash)]
+pub struct UpdateUnits;
 
-        let collider = LocalCollider::new(
-            ObjectCollider::from(TriMesh::from(Cuboid::new(Vector::new(3., 3., 4.)))),
-            Isometry::new(Vector::new(x, 0., y), Vector::identity()),
-        );
-
-        let entity = world
-            .spawn((
-                Transform::from_xyz(x, y, 0.0),
-                EnergyReceiver,
-                NearbyUnits::default(),
-            ))
-            .id();
-
-        index.insert(entity, collider);
-    }
-    world.insert_resource(index);
-}
+#[derive(Component)]
+struct UnitNumber(u32);
 
 fn update_index(
     mut index: ResMut<EntityIndex>,
-    moved: Query<(Entity, &Transform), (Changed<Transform>)>,
+    moved: Query<(Entity, &Transform), Changed<Transform>>,
 ) {
     for (entity, transform) in moved.iter() {
         let position = Isometry::new(
@@ -47,30 +37,30 @@ fn update_index(
     }
 }
 
-fn teleport_entities_randomly(mut query: Query<&mut Transform>) {
-    for mut transform in query.iter_mut() {
-        // we just move the entities somewhere in a random direction
-        let offset_x: f32 = fastrand::i32(1..3) as f32;
-        let offset_y: f32 = fastrand::i32(1..3) as f32;
+fn load_points(number: u32) -> Vec<Vec2> {
+    let mut points_path: PathBuf = env!("CARGO_MANIFEST_DIR").into();
+    points_path.push("test_data");
+    points_path.push(format!("{number}-points.txt"));
+    let reader = BufReader::new(File::open(points_path).unwrap());
 
-        transform.translation.x += offset_x;
-        transform.translation.y += offset_y;
+    let mut points = Vec::with_capacity(number as usize);
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let mut numbers = line.split_whitespace();
+        let x: f32 = numbers.next().unwrap().parse().unwrap();
+        let y: f32 = numbers.next().unwrap().parse().unwrap();
+        points.push((MAP_SIZE - DISTANCE_FROM_MAP_EDGE) * Vec2::new(x, y));
     }
+    points
 }
 
-// moving but deciding a direction and traviling for one second before deciding a new direction
-#[derive(Component)]
-struct Direction {
-    x: f32,
-    y: f32,
-}
-
-fn init_world_with_entities_moving(world: &mut World, num_entities: usize) {
+fn init_world_with_entities_moving(world: &mut World, num_entities: u32) {
     let mut index = EntityIndex::new();
+    let points = load_points(num_entities);
 
-    for _ in 0..num_entities {
-        let x: f32 = fastrand::i32(-500..500) as f32;
-        let y: f32 = fastrand::i32(-500..500) as f32;
+    for (i, point) in points.into_iter().enumerate() {
+        let x: f32 = point.x;
+        let y: f32 = point.y;
 
         let collider = LocalCollider::new(
             ObjectCollider::from(TriMesh::from(Cuboid::new(Vector::new(3., 3., 4.)))),
@@ -82,32 +72,34 @@ fn init_world_with_entities_moving(world: &mut World, num_entities: usize) {
                 Transform::from_xyz(x, y, 0.0),
                 EnergyReceiver,
                 NearbyUnits::default(),
-                Direction { x: 0., y: 0. },
+                UnitNumber(i as u32),
             ))
             .id();
 
         index.insert(entity, collider);
     }
+
     world.insert_resource(index);
 }
 
-#[derive(Resource)]
-struct DirectionTimer(Timer);
+/// Move entities in circles of radius N / 2.
+fn move_entities_in_circle(time: Res<Time>, mut query: Query<&mut Transform>) {
+    for mut transform in query.iter_mut() {
+        // Change direction (counter)clockwise based on entity_mum % 2 == 0
+        let direction = if transform.translation.x % 2. == 0. {
+            1.
+        } else {
+            -1.
+        };
 
-fn move_entities_randomly_direction(
-    mut query: Query<(&mut Transform, &mut Direction)>,
-    mut timer: ResMut<DirectionTimer>,
-    mut time: ResMut<Time>,
-) {
-    for (mut transform, mut direction) in query.iter_mut() {
-        timer.0.tick(time.delta());
-        if timer.0.just_finished() {
-            direction.x = fastrand::i32(-1..2) as f32;
-            direction.y = fastrand::i32(-1..2) as f32;
-        }
+        let t = time.elapsed_seconds();
+        let radius = DISTANCE_FROM_MAP_EDGE / 2.;
 
-        transform.translation.x += direction.x*time.delta_seconds();
-        transform.translation.y += direction.y*time.delta_seconds();
+        let x = radius * (t * 0.5 * direction).sin();
+        let y = radius * (t * 0.5 * direction).cos();
+
+        transform.translation.x = x;
+        transform.translation.y = y;
     }
 }
 
@@ -116,41 +108,30 @@ fn nearby_benchmark(c: &mut Criterion) {
 
     for i in [100, 1_000, 10_000].iter() {
         let mut app = App::default();
-        init_world_with_entities(&mut app.world, *i);
-        app.add_systems(
-            Update,
-            (teleport_entities_randomly, update_nearby_recv, update_index),
-        );
-
-        group.throughput(criterion::Throughput::Elements(*i as u64));
-        group.bench_function(format!("{} entities all teleporting randomly", i), |b| {
-            b.iter(|| {
-                app.update();
-            })
-        });
-    }
-
-    for i in [100, 1_000, 10_000].iter() {
-        let mut app = App::default();
         init_world_with_entities_moving(&mut app.world, *i);
-        app.add_systems(
-            Update,
-            (
-                move_entities_randomly_direction,
-                update_nearby_recv,
-                update_index,
-            ),
-        );
+        app.add_systems(Update, (update_nearby_recv,));
         app.add_plugins(TimePlugin);
-        app.insert_resource(DirectionTimer(Timer::from_seconds(
-            1.0,
-            TimerMode::Repeating,
-        )));
+
+        let update_units_schedule = Schedule::default();
+        app.add_schedule(UpdateUnits, update_units_schedule);
+
+        app.add_systems(UpdateUnits, (update_index, move_entities_in_circle));
 
         group.throughput(criterion::Throughput::Elements(*i as u64));
-        group.bench_function(format!("{} entities all moving randomly", i), |b| {
-            b.iter(|| {
-                app.update();
+        group.bench_function(format!("{} entities all moving in circles", i), |b| {
+            b.iter_custom(|iters| {
+                let time = std::time::Instant::now();
+                let mut duration_updating_units = std::time::Duration::default();
+
+                for _ in 0..iters {
+                    let update_unit_time = std::time::Instant::now();
+                    app.world.run_schedule(UpdateUnits);
+                    duration_updating_units += update_unit_time.elapsed();
+
+                    app.update();
+                }
+
+                time.elapsed() - duration_updating_units
             })
         });
     }
