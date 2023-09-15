a808c94e63788fdaac07bad011ca398a7975cf1c
Martin Indra <martin.indra@mgn.cz>
2023-09-15 23:43:58 +0200

    de_messages: Remove an unnecessary impl (#724)
    No need to make the index transparent as long as once can easily use the
    entity directly in e.g. a hash map.

--- crates/messages/src/players/entity.rs
+++ crates/messages/src/players/entity.rs
@@ -3,14 +3,8 @@ use bevy::ecs::entity::Entity;
 use bincode::{Decode, Encode};
 
 /// Bevy ECS Entity derived identification of an entity.
-#[derive(Debug, Encode, Decode)]
+#[derive(Clone, Copy, Debug, Encode, Decode, Hash, PartialEq, Eq)]
 pub struct EntityNet(u32);
-
-impl EntityNet {
-    pub fn index(&self) -> u32 {
-        self.0
-    }
-}
 
 #[cfg(feature = "bevy")]
 impl From<Entity> for EntityNet {
